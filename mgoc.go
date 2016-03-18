55b879ed1f2eda884b32b69bccfbb594bbbb9bf9
Олексій Чечель <alex.mirrr@gmail.com>
2016-03-18 12:42:48 +0700

    Add groups of IDs

--- mgoc.go
+++ mgoc.go
@@ -1,9 +1,11 @@
 package mgoc
 
 import (
+	"fmt"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mirrr/types.v1"
 	"reflect"
+	"strings"
 	"sync"
 	"time"
 )
@@ -18,9 +20,12 @@ type (
 		collection *mgo.Collection
 		sliceType  reflect.Type
 		el         interface{}
-		key        string
+		idField    string
 		pass       bool
+		runned     bool
 		query      interface{}
+		path       []string
+		pathObj    obj
 		sync.Mutex
 	}
 
@@ -29,11 +34,11 @@ type (
 	}
 )
 
-func New(collection *mgo.Collection, el interface{}, key string) *Cache {
+func New(collection *mgo.Collection, el interface{}, idField string) *Cache {
 	return &Cache{
 		collection: collection,
 		sliceType:  reflect.SliceOf(reflect.TypeOf(el)),
-		key:        key,
+		idField:    idField,
 		el:         el,
 		query:      obj{},
 		interval:   time.Second,
@@ -45,18 +50,29 @@ func (c *Cache) Query(query interface{}) *Cache {
 	return c
 }
 
+func (c *Cache) Group(path string) *Cache {
+	c.path = strings.Split(path, "/")
+	return c
+}
+
 func (c *Cache) Interval(duration time.Duration) *Cache {
 	c.interval = duration
 	return c
 }
 
 func (c *Cache) Run() *Cache {
-	c.Update()
-	go func() {
-		for range time.Tick(c.interval) {
-			c.Update()
-		}
-	}()
+	if !c.runned {
+		c.runned = true
+		c.Update()
+		go func() {
+			for range time.Tick(c.interval) {
+				if !c.runned {
+					return
+				}
+				c.Update()
+			}
+		}()
+	}
 	return c
 }
 
@@ -67,26 +83,89 @@ func (c *Cache) Update() {
 
 		ptr := reflect.New(c.sliceType)
 		ptr.Elem().Set(reflect.MakeSlice(c.sliceType, 0, 0))
-		list := ptr.Interface()
+		array := ptr.Interface()
 
-		if err := c.collection.Find(c.query).All(list); err != nil {
+		if err := c.collection.Find(c.query).All(array); err != nil {
+			fmt.Println(err)
 			return
 		}
 
 		c.Lock()
 		defer c.Unlock()
 		c.list = obj{}
-		listV := reflect.ValueOf(list).Elem()
-		for i := 0; i < listV.Len(); i++ {
-			current := listV.Index(i)
-			id := types.String(reflect.Indirect(current).FieldByName(c.key))
+		c.pathObj = obj{}
+		arrayVl := reflect.ValueOf(array).Elem()
+		for i := 0; i < arrayVl.Len(); i++ {
+			current := arrayVl.Index(i)
+			idval := reflect.Indirect(current).FieldByName(c.idField)
+			if idval.Kind() == reflect.Invalid {
+				panic("Invalid ID struct field: " + c.idField)
+			}
+			id := types.String(idval)
 			c.list[id] = current.Interface()
+
+			// рекурсивная группировка по полям структуры
+			if len(c.path) > 0 {
+				c.branch(c.pathObj, current, id, 0)
+			}
 		}
 	}
 }
 
-func (c *Cache) Get(key interface{}) interface{} {
+func (c *Cache) branch(po interface{}, current reflect.Value, id string, k int) {
+	v := c.path[k]
+	last := k+1 == len(c.path)
+	if len(v) > 0 {
+		process := func(val string) {
+			if !last {
+				if _, exists := po.(obj)[val]; !exists {
+					po.(obj)[val] = obj{}
+				}
+				c.branch(po.(obj)[val], current, id, k+1)
+			} else {
+				if _, exists := po.(obj)[val]; !exists {
+					po.(obj)[val] = arr{}
+				}
+				po.(obj)[val] = append(po.(obj)[val].(arr), id)
+			}
+		}
+
+		fval := reflect.Indirect(current).FieldByName(v)
+		if fval.Kind() == reflect.Invalid {
+			panic("Invalid struct field: " + v)
+		} else if fval.Kind() == reflect.Slice {
+			for j := 0; j < fval.Len(); j++ {
+				process(types.String(fval.Index(j)))
+			}
+		} else {
+			process(types.String(fval))
+		}
+	}
+}
+
+func (c *Cache) Get(id interface{}) interface{} {
 	c.Lock()
 	defer c.Unlock()
-	return c.list[types.String(key)]
+	return c.list[types.String(id)]
+}
+
+func (c *Cache) Len() int {
+	c.Lock()
+	defer c.Unlock()
+	return len(c.list)
+}
+
+func (c *Cache) GetIDsByPath(key string) (ret interface{}) {
+	keys := strings.Split(key, "/")
+	ret = []string{}
+
+	c.Lock()
+	defer c.Unlock()
+
+	if len(c.pathObj) < len(keys) {
+		return
+	}
+
+	ret = types.MGet(c.pathObj, types.Slice(keys)...)
+	return
 }
